d2efd222563c45d221740893111b0aa1d6b48ceb
foint4it <walter.delafuente@gmail.com>
2022-03-30 13:14:13 -0300

    sigo settings 2

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -172,8 +172,10 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'blog/static')
 ]
 
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = BASE_DIR.child('media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR.child('media')
 
 # ckeditor settings
 CKEDITOR_UPLOAD_PATH = 'uploads/'
